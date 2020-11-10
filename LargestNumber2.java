79b15628a45b6432f8175d29f37f35a65ccfdb1b
Tayfun <tayfunymz@gmail.com>
2020-11-10 10:20:06 -0800

    Initial commit

---
+++ LargestNumber2.java
@@ -0,0 +1,40 @@
+package Week03;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class LargestNumber2 {
+    private static ArrayList<String> swap(ArrayList<String> digits, int i, int j) {
+        Collections.swap(digits, i, j);
+        return digits;
+    }
+    private static boolean compare(String i, String j) {
+        String iJ = i + j;
+        String Ji = j + i;
+        return (Integer.parseInt(iJ) > Integer.parseInt(Ji));
+    }
+    private static String largestNumber(ArrayList<String> digits) {
+        String answer = "";
+        while (digits.size() != 0) {
+            int i = 0;
+            for (int j = i + 1; j < digits.size(); j++) {
+                if (!compare(digits.get(i), digits.get(j))){
+                    swap(digits, i, j);
+                }
+            }
+            answer += digits.get(0);
+            digits.remove(0);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<String> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add(i, scanner.next());
+        }
+        System.out.println(largestNumber(a));
+    }
+}
---
+++ PrimeNumbers.java
@@ -0,0 +1,34 @@
+package Week2;
+import java.util.ArrayList;
+
+public class PrimeNumbers {
+    public static ArrayList<Integer> getList(int max) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int n = max;
+        int index = 2;
+        for (int i = 0; i < n; i++) {
+            numbers.add(index);
+            index++;
+        }
+        return numbers;
+    }
+    public static ArrayList<Integer> getPrimes(ArrayList<Integer> numbers) {
+        ArrayList<Integer> primes = new ArrayList<>();
+
+        while(numbers != null) {
+            if (numbers.size() == 0) { break; }
+            int prime = numbers.get(0);
+            for (int i = 0; i < numbers.size(); i++){
+                if (numbers.get(i) % prime == 0) {
+                    numbers.remove(i);
+                }
+            }
+            primes.add(prime);
+        }
+        return primes;
+    }
+    public static void main(String[] args) {
+        int maxValue = 1000;
+        System.out.println(getPrimes(getList(maxValue)));
+    }
+}
